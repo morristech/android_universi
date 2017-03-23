1a8a4d7cf7b6e7bc5838e2728573b44d144c1dd4
Martin Albedinsky <martin.albedinsky@gmail.com>
2017-03-23 19:49:45 +0100

    Merged from the framework branch.

--- library-core/src/androidTest/java/universum/studios/android/support/universi/CoreTests.java
+++ library-core/src/androidTest/java/universum/studios/android/support/universi/CoreTests.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  * =================================================================================================
  */
-package universum.studios.android.universi;
+package universum.studios.android.support.universi;
 
 /**
  * @author Martin Albedinsky
