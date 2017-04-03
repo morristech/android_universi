8ac2838ee2f49eec581e8372bd6a95fb8e6b3f02
Martin Albedinsky <martin.albedinsky@gmail.com>
2017-04-03 09:50:55 +0200

    #1 IN PROGRESS

--- library-fragment/src/androidTest/java/universum/studios/android/universi/UniversiFragmentTest.java
+++ library-fragment/src/androidTest/java/universum/studios/android/universi/UniversiFragmentTest.java
@@ -4,20 +4,19 @@
  * =================================================================================================
  *         Licensed under the Apache License, Version 2.0 or later (further "License" only).
  * -------------------------------------------------------------------------------------------------
- * You may use this file only in compliance with the License. More details and copy of this License
+ * You may use this file only in compliance with the License. More details and copy of this License 
  * you may obtain at
- *
+ * 
  * 		http://www.apache.org/licenses/LICENSE-2.0
- *
- * You can redistribute, modify or publish any part of the code written within this file but as it
- * is described in the License, the software distributed under the License is distributed on an
+ * 
+ * You can redistribute, modify or publish any part of the code written within this file but as it 
+ * is described in the License, the software distributed under the License is distributed on an 
  * "AS IS" BASIS, WITHOUT WARRANTIES or CONDITIONS OF ANY KIND.
- *
+ * 
  * See the License for the specific language governing permissions and limitations under the License.
  * =================================================================================================
  */
-package universum.studios.android.universi;
-
+package universum.studios.android.universi; 
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
@@ -29,12 +28,12 @@ import universum.studios.android.test.BaseInstrumentedTest;
  * @author Martin Albedinsky
  */
 @RunWith(AndroidJUnit4.class)
-public final class UniversiActivityDelegateTest extends BaseInstrumentedTest {
-
+public final class UniversiFragmentTest extends BaseInstrumentedTest {
+    
 	@SuppressWarnings("unused")
-	private static final String TAG = "UniversiActivityDelegateTest";
+	private static final String TAG = "UniversiFragmentTest";
 
-	@Test
+    @Test
 	public void test() {
 		// todo:: implement test
 	}
