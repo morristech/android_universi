6633030e8cc866daa1a86cafbd31097dbc47ce46
Martin Albedinsky <martin.albedinsky@gmail.com>
2017-06-03 21:59:24 +0200

    #1 IN PROGRESS

--- library-core/src/androidTest/java/universum/studios/android/universi/UniversiContextDelegateTest.java
+++ library-core/src/androidTest/java/universum/studios/android/universi/UniversiContextDelegateTest.java
@@ -16,8 +16,15 @@
  * See the License for the specific language governing permissions and limitations under the License.
  * =================================================================================================
  */
-package universum.studios.android.universi; 
+package universum.studios.android.universi;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -25,14 +32,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import universum.studios.android.dialog.manage.DialogController;
+import universum.studios.android.dialog.manage.DialogFactory;
+import universum.studios.android.dialog.manage.DialogXmlFactory;
 import universum.studios.android.test.BaseInstrumentedTest;
 import universum.studios.android.test.TestActivity;
 import universum.studios.android.test.TestFragment;
+import universum.studios.android.test.TestResources;
+import universum.studios.android.test.TestUtils;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assume.assumeTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Martin Albedinsky
@@ -60,17 +79,263 @@ public final class UniversiContextDelegateTest extends BaseInstrumentedTest {
 
 	@Test
 	public void testSetGetDialogController() {
-		final UniversiContextDelegate delegate = new TestDelegate(mContext);
-		final DialogController controller = new DialogController(mock(TestActivity.class));
-		delegate.setDialogController(controller);
-		assertThat(delegate.getDialogController(), is(controller));
+		final UniversiContextDelegate delegate = new TestDelegate();
+		final DialogController mockController = mock(DialogController.class);
+		final DialogFactory mockFactory = mock(DialogFactory.class);
+		delegate.setDialogFactory(mockFactory);
+		delegate.setDialogController(mockController);
+		assertThat(delegate.getDialogController(), is(mockController));
+		verify(mockController, times(1)).setFactory(mockFactory);
+		verifyNoMoreInteractions(mockController);
 	}
 
 	@Test
 	public void testSetNullDialogController() {
-		final UniversiContextDelegate delegate = new TestDelegate(mContext);
+		final UniversiContextDelegate delegate = new TestDelegate();
 		delegate.setDialogController(null);
 		assertThat(delegate.getDialogController(), is(notNullValue()));
+	}
+
+	@Test
+	public void testSetGetDialogFactory() {
+		final UniversiContextDelegate delegate = new TestDelegate();
+		final DialogFactory mockFactory = mock(DialogFactory.class);
+		delegate.setDialogFactory(mockFactory);
+		assertThat(delegate.getDialogFactory(), is(mockFactory));
+	}
+
+	@Test
+	public void testSetDialogXmlFactory() {
+		assumeTrue(TestUtils.hasLibraryRootPackageName(mContext));
+		final UniversiContextDelegate delegate = new TestDelegate();
+		delegate.setDialogXmlFactory(TestResources.resourceIdentifier(mContext, TestResources.XML, "dialogs"));
+		assertThat(delegate.getDialogFactory(), is(notNullValue()));
+		assertThat(delegate.getDialogFactory(), instanceOf(DialogXmlFactory.class));
+	}
+
+	@Test
+	public void testSetDialogXmlFactoryWithZeroResource() {
+		final UniversiContextDelegate delegate = new TestDelegate();
+		delegate.setDialogFactory(mock(DialogFactory.class));
+		delegate.setDialogXmlFactory(0);
+		assertThat(delegate.getDialogFactory(), is(nullValue()));
+	}
+
+	@Test
+	public void testShowDialogWithId() {
+		final TestActivity mockActivity = mock(TestActivity.class);
+		final FragmentManager mockFragmentManager = mock(FragmentManager.class);
+		final DialogFragment mockDialogFragment = mock(DialogFragment.class);
+		when(mockActivity.getFragmentManager()).thenReturn(mockFragmentManager);
+		final UniversiContextDelegate delegate = new TestDelegate(mockActivity);
+		final DialogFactory mockFactory = mock(DialogFactory.class);
+		when(mockFactory.isDialogProvided(1)).thenReturn(true);
+		when(mockFactory.createDialogTag(1)).thenReturn("Dialog.TAG.1");
+		when(mockFactory.createDialog(1, null)).thenReturn(mockDialogFragment);
+		delegate.setDialogFactory(mockFactory);
+		assertThat(delegate.showDialogWithId(1, null), is(true));
+		verify(mockFactory, times(1)).isDialogProvided(1);
+		verify(mockFactory, times(1)).createDialogTag(1);
+		verify(mockFactory, times(1)).createDialog(1, null);
+		verifyNoMoreInteractions(mockFactory);
+	}
+
+	@Test
+	public void testShowDialogWithIdWhenPaused() {
+		final UniversiContextDelegate delegate = new TestDelegate();
+		final DialogFactory mockFactory = mock(DialogFactory.class);
+		delegate.setDialogFactory(mockFactory);
+		delegate.setPaused(true);
+		assertThat(delegate.showDialogWithId(1, null), is(false));
+		verifyZeroInteractions(mockFactory);
+	}
+
+	@Test
+	public void testShowDialogWithIdWithoutDialogFactoryAttached() {
+		assertThat(new TestDelegate(mContext).showDialogWithId(1, null), is(false));
+	}
+
+	@Test
+	public void testDismissDialogWithId() {
+		final TestActivity mockActivity = mock(TestActivity.class);
+		final FragmentManager mockFragmentManager = mock(FragmentManager.class);
+		final DialogFragment mockDialogFragment = mock(DialogFragment.class);
+		when(mockFragmentManager.findFragmentByTag("Dialog.TAG.1")).thenReturn(mockDialogFragment);
+		when(mockActivity.getFragmentManager()).thenReturn(mockFragmentManager);
+		final UniversiContextDelegate delegate = new TestDelegate(mockActivity);
+		final DialogFactory mockFactory = mock(DialogFactory.class);
+		when(mockFactory.isDialogProvided(1)).thenReturn(true);
+		when(mockFactory.createDialogTag(1)).thenReturn("Dialog.TAG.1");
+		delegate.setDialogFactory(mockFactory);
+		assertThat(delegate.dismissDialogWithId(1), is(true));
+		verify(mockFactory, times(2)).isDialogProvided(1);
+		verify(mockFactory, times(2)).createDialogTag(1);
+		verifyNoMoreInteractions(mockFactory);
+	}
+
+	@Test
+	public void testDismissDialogWithIdWhichIsNotShowing() {
+		final TestActivity mockActivity = mock(TestActivity.class);
+		final FragmentManager mockFragmentManager = mock(FragmentManager.class);
+		when(mockFragmentManager.findFragmentByTag("Dialog.TAG.1")).thenReturn(null);
+		when(mockActivity.getFragmentManager()).thenReturn(mockFragmentManager);
+		final UniversiContextDelegate delegate = new TestDelegate(mockActivity);
+		final DialogFactory mockFactory = mock(DialogFactory.class);
+		when(mockFactory.isDialogProvided(1)).thenReturn(true);
+		when(mockFactory.createDialogTag(1)).thenReturn("Dialog.TAG.1");
+		delegate.setDialogFactory(mockFactory);
+		assertThat(delegate.dismissDialogWithId(1), is(false));
+		verify(mockFactory, times(2)).isDialogProvided(1);
+		verify(mockFactory, times(1)).createDialogTag(1);
+		verifyNoMoreInteractions(mockFactory);
+	}
+
+	@Test
+	public void testDismissDialogWithIdWhenPaused() {
+		final UniversiContextDelegate delegate = new TestDelegate();
+		final DialogFactory mockFactory = mock(DialogFactory.class);
+		delegate.setDialogFactory(mockFactory);
+		delegate.setPaused(true);
+		assertThat(delegate.dismissDialogWithId(1), is(false));
+		verifyZeroInteractions(mockFactory);
+	}
+
+	@Test
+	public void testDismissDialogWithIdWithoutDialogFactoryAttached() {
+		assertThat(new TestDelegate(mContext).dismissDialogWithId(1), is(false));
+	}
+
+	@Test
+	public void testShowXmlDialog() {
+		// Ignored, because we would need to import whole dialogs library in order to successfully
+		// perform this test.
+	}
+
+	@Test
+	public void testShowXmlDialogWhenPaused() {
+		assumeTrue(TestUtils.hasLibraryRootPackageName(mContext));
+		final UniversiContextDelegate delegate = new TestDelegate();
+		delegate.setPaused(true);
+		assertThat(delegate.showXmlDialog(TestResources.resourceIdentifier(mContext, TestResources.XML, "dialog"), null), is(false));
+	}
+
+	@Test
+	public void testDismissXmlDialog() {
+		assumeTrue(TestUtils.hasLibraryRootPackageName(mContext));
+		final int dialogResource = TestResources.resourceIdentifier(mContext, TestResources.XML, "dialog");
+		final TestActivity mockActivity = mock(TestActivity.class);
+		final FragmentManager mockFragmentManager = mock(FragmentManager.class);
+		final DialogFragment mockDialogFragment = mock(DialogFragment.class);
+		when(mockFragmentManager.findFragmentByTag(DialogXmlFactory.class.getName() + ".TAG." + dialogResource)).thenReturn(mockDialogFragment);
+		when(mockActivity.getFragmentManager()).thenReturn(mockFragmentManager);
+		final UniversiContextDelegate delegate = new TestDelegate(mockActivity);
+		assertThat(delegate.dismissXmlDialog(dialogResource), is(true));
+	}
+
+	@Test
+	public void testDismissXmlDialogWhichIsNotShowing() {
+		assumeTrue(TestUtils.hasLibraryRootPackageName(mContext));
+		final int dialogResource = TestResources.resourceIdentifier(mContext, TestResources.XML, "dialog");
+		final TestActivity mockActivity = mock(TestActivity.class);
+		final FragmentManager mockFragmentManager = mock(FragmentManager.class);
+		when(mockFragmentManager.findFragmentByTag(DialogXmlFactory.class.getName() + ".TAG." + dialogResource)).thenReturn(null);
+		when(mockActivity.getFragmentManager()).thenReturn(mockFragmentManager);
+		final UniversiContextDelegate delegate = new TestDelegate(mockActivity);
+		assertThat(delegate.dismissXmlDialog(dialogResource), is(false));
+	}
+
+	@Test
+	public void testDismissXmlDialogWhichIsNotDialogFragment() {
+		assumeTrue(TestUtils.hasLibraryRootPackageName(mContext));
+		final int dialogResource = TestResources.resourceIdentifier(mContext, TestResources.XML, "dialog");
+		final TestActivity mockActivity = mock(TestActivity.class);
+		final FragmentManager mockFragmentManager = mock(FragmentManager.class);
+		final Fragment mockFragment = mock(TestFragment.class);
+		when(mockFragmentManager.findFragmentByTag(DialogXmlFactory.class.getName() + ".TAG." + dialogResource)).thenReturn(mockFragment);
+		when(mockActivity.getFragmentManager()).thenReturn(mockFragmentManager);
+		final UniversiContextDelegate delegate = new TestDelegate(mockActivity);
+		assertThat(delegate.dismissXmlDialog(dialogResource), is(false));
+	}
+
+	@Test
+	public void testDismissXmlDialogWhenPaused() {
+		assumeTrue(TestUtils.hasLibraryRootPackageName(mContext));
+		final UniversiContextDelegate delegate = new TestDelegate();
+		delegate.setPaused(true);
+		assertThat(delegate.dismissXmlDialog(TestResources.resourceIdentifier(mContext, TestResources.XML, "dialog")), is(false));
+	}
+
+	@Test
+	public void testIsActiveNetworkConnected() {
+		final Context mockContext = mock(Context.class);
+		final ConnectivityManager mockConnectivityManager = mock(ConnectivityManager.class);
+		final NetworkInfo mockNetworkInfo = mock(NetworkInfo.class);
+		when(mockNetworkInfo.isConnected()).thenReturn(true);
+		when(mockConnectivityManager.getActiveNetworkInfo()).thenReturn(mockNetworkInfo);
+		when(mockContext.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(mockConnectivityManager);
+		when(mockContext.getApplicationContext()).thenReturn(mockContext);
+		final UniversiContextDelegate delegate = new TestDelegate(mockContext);
+		assertThat(delegate.isActiveNetworkConnected(), is(true));
+		assertThat(delegate.isActiveNetworkConnected(), is(true));
+	}
+
+	@Test
+	public void testIsActiveNetworkConnectedWhenThereIsNone() {
+		final Context mockContext = mock(Context.class);
+		final ConnectivityManager mockConnectivityManager = mock(ConnectivityManager.class);
+		when(mockConnectivityManager.getActiveNetworkInfo()).thenReturn(null);
+		when(mockContext.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(mockConnectivityManager);
+		when(mockContext.getApplicationContext()).thenReturn(mockContext);
+		assertThat(new TestDelegate(mockContext).isActiveNetworkConnected(), is(false));
+	}
+
+	@Test
+	public void testIsActiveNetworkConnectedWhenDisconnected() {
+		final Context mockContext = mock(Context.class);
+		final ConnectivityManager mockConnectivityManager = mock(ConnectivityManager.class);
+		final NetworkInfo mockNetworkInfo = mock(NetworkInfo.class);
+		when(mockNetworkInfo.isConnected()).thenReturn(false);
+		when(mockConnectivityManager.getActiveNetworkInfo()).thenReturn(mockNetworkInfo);
+		when(mockContext.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(mockConnectivityManager);
+		when(mockContext.getApplicationContext()).thenReturn(mockContext);
+		assertThat(new TestDelegate(mockContext).isActiveNetworkConnected(), is(false));
+	}
+
+	@Test
+	@SuppressWarnings("deprecation")
+	public void testIsNetworkConnected() {
+		final Context mockContext = mock(Context.class);
+		final ConnectivityManager mockConnectivityManager = mock(ConnectivityManager.class);
+		final NetworkInfo mockNetworkInfo = mock(NetworkInfo.class);
+		when(mockNetworkInfo.isConnected()).thenReturn(true);
+		when(mockConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)).thenReturn(mockNetworkInfo);
+		when(mockContext.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(mockConnectivityManager);
+		when(mockContext.getApplicationContext()).thenReturn(mockContext);
+		assertThat(new TestDelegate(mockContext).isNetworkConnected(ConnectivityManager.TYPE_MOBILE), is(true));
+	}
+
+	@Test
+	@SuppressWarnings("deprecation")
+	public void testIsNetworkConnectedWhenThereIsNone() {
+		final Context mockContext = mock(Context.class);
+		final ConnectivityManager mockConnectivityManager = mock(ConnectivityManager.class);
+		when(mockConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)).thenReturn(null);
+		when(mockContext.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(mockConnectivityManager);
+		when(mockContext.getApplicationContext()).thenReturn(mockContext);
+		assertThat(new TestDelegate(mockContext).isNetworkConnected(ConnectivityManager.TYPE_MOBILE), is(false));
+	}
+
+	@Test
+	@SuppressWarnings("deprecation")
+	public void testIsNetworkConnectedWhenDisconnected() {
+		final Context mockContext = mock(Context.class);
+		final ConnectivityManager mockConnectivityManager = mock(ConnectivityManager.class);
+		final NetworkInfo mockNetworkInfo = mock(NetworkInfo.class);
+		when(mockNetworkInfo.isConnected()).thenReturn(false);
+		when(mockConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)).thenReturn(mockNetworkInfo);
+		when(mockContext.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(mockConnectivityManager);
+		when(mockContext.getApplicationContext()).thenReturn(mockContext);
+		assertThat(new TestDelegate(mockContext).isNetworkConnected(ConnectivityManager.TYPE_MOBILE), is(false));
 	}
 
 	@Test
@@ -118,6 +383,17 @@ public final class UniversiContextDelegateTest extends BaseInstrumentedTest {
 
     private static class TestDelegate extends UniversiContextDelegate {
 
+	    private Activity activity;
+
+	    TestDelegate() {
+		    this(mock(TestActivity.class));
+	    }
+
+	    TestDelegate(Activity activity) {
+		    this((Context) activity);
+		    this.activity = activity;
+	    }
+
 	    TestDelegate(@NonNull Context context) {
 		    super(context);
 	    }
@@ -125,7 +401,7 @@ public final class UniversiContextDelegateTest extends BaseInstrumentedTest {
 	    @NonNull
 	    @Override
 	    DialogController instantiateDialogController() {
-		    return new DialogController(mock(TestActivity.class));
+		    return new DialogController(activity);
 	    }
     }
 }
